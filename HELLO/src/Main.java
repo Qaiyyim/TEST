6b46648b2d41dc11403cc2115f3543c700cfb3d8
owner <owner@LAPTOP-0P3VDQEV>
2016-09-19 16:00:02 +0800

    lol
    Change-Id: Ib9193788a0811e6077fcb6520146fd488154c35b

--- HELLO/src/Main.java
+++ HELLO/src/Main.java
@@ -3,7 +3,7 @@ class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.println("Testing push 333333");
+		System.out.println("Testing push 3333733");
 
 	}
 
