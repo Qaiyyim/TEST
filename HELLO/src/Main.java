445853de14badbcfaf47890de76fadfd1db617de
qai <owner@LAPTOP-0P3VDQEV>
2016-09-19 16:10:22 +0800

    454545

--- HELLO/src/Main.java
+++ HELLO/src/Main.java
@@ -3,7 +3,7 @@ class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.println("Testing push 444");
+		System.out.println("Testing push 50560560064");
 
 	}
 
