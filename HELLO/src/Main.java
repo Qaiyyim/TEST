01144c1a86649c40f9b875e68cd85fd2a28f2b25
owner <owner@LAPTOP-0P3VDQEV>
2016-09-19 15:50:33 +0800

    QAIYAYYAYAYYAYAUY
    Change-Id: I5690d39597135fe73c539e07516f66ebd27041f1

--- HELLO/src/Main.java
+++ HELLO/src/Main.java
@@ -3,7 +3,7 @@ class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.println("Testing push 2");
+		System.out.println("Testing push 333333");
 
 	}
 
